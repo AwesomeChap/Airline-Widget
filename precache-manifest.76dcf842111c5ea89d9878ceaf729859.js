00317016b075805f39be773d88ea77a9071be108
Jatin Kumar <jatin15011999@gmail.com>
2019-02-08 00:38:56 +0530

    Updates

---
+++ precache-manifest.76dcf842111c5ea89d9878ceaf729859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c65c2bef6dd95f32ba",
+    "url": "/AwesomeChap/Airline-Widget/static/js/runtime~main.f5c65c2b.js"
+  },
+  {
+    "revision": "5d4c54427001161be9f4",
+    "url": "/AwesomeChap/Airline-Widget/static/js/main.5d4c5442.chunk.js"
+  },
+  {
+    "revision": "ebf9d6aa2eb74cc89bd3",
+    "url": "/AwesomeChap/Airline-Widget/static/js/1.ebf9d6aa.chunk.js"
+  },
+  {
+    "revision": "5d4c54427001161be9f4",
+    "url": "/AwesomeChap/Airline-Widget/static/css/main.6d06466c.chunk.css"
+  },
+  {
+    "revision": "5d574fc25dd48f9c879c1de552665a5f",
+    "url": "/AwesomeChap/Airline-Widget/index.html"
+  }
+];
